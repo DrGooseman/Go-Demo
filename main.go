3f06b33960caa5f69eb4c465cadd29af3f161047
DrGooseman <jamesjquinn12@gmail.com>
2020-05-14 11:52:46 +0300

    Go practice initial commit + basic variable declarations

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, world!")
+
+	var x int = 5
+	y := 7
+	var sum int
+	sum = x + y
+
+	fmt.Println(sum)
+
+	var a [5]int
+	fmt.Println(a)
+
+
+}
