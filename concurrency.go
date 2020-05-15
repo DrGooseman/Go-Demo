b494641e5d5b8f450ef17c8f7cf1b99b1b6f4093
DrGooseman <jamesjquinn12@gmail.com>
2020-05-15 15:12:50 +0300

    adds more concurrency

--- concurrency.go
+++ concurrency.go
@@ -7,6 +7,49 @@ import (
 )
 
 func main() {
+
+		// channels
+
+		c := make(chan string)
+		go count2("sheep", c)
+		
+		for msg := range c {
+			fmt.Println(msg)
+		}
+
+// 2 channels
+
+c1 := make(chan string)
+c2 := make(chan string)
+
+go func() {
+	for {
+		c1 <- "Every 500ms"
+		time.Sleep(time.Millisecond * 500)
+	}
+} ()
+
+go func() {
+	for {
+		c2 <- "Every 2 seconds"
+		time.Sleep(time.Second * 2)
+	}
+} ()
+
+for {
+	select {
+	case msg1 := <- c1:
+		fmt.Println(msg1)
+	case msg2 := <- c2:
+		fmt.Println(msg2)
+	}
+}
+
+
+
+
+		// WaitGroup
+
 	var wg sync.WaitGroup
 	wg.Add(1)
 
@@ -28,4 +71,13 @@ func count(thing string) {
 		fmt.Println(i, thing)
 		time.Sleep(time.Millisecond * 500)
 	}
+}
+
+func count2(thing string, c chan string) {
+	for i := 1; i <= 5; i++ {
+		c <- thing
+		time.Sleep(time.Millisecond * 500)
+	}
+
+	close(c)
 }
